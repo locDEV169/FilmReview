eaf21b3b9bf7235be3cd8d4f1c766cc551eb54d7
locDEV169 <48356607+locDEV169@users.noreply.github.com>
2021-09-10 09:29:50 +0700

    1st cmnt

---
+++ src/main/java/com/filmreview/springMVCbackend/SpringMvcBackendApplication.java
@@ -0,0 +1,13 @@
+package com.filmreview.springMVCbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMvcBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/filmreview/springMVCbackend/SpringMvcBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.filmreview.springMVCbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvcBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
